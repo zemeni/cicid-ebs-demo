26908af90bba1cab5d97654abbac89c3c8f2cd0b
Babu <neupanebabu828@gmail.com>
2020-08-21 01:39:17 +1000

    added GetMapping annotation

--- src/main/java/com/babu/cicd/HelloController.java
+++ src/main/java/com/babu/cicd/HelloController.java
@@ -1,10 +1,12 @@
 package com.babu.cicd;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/hello")
 public class HelloController {
 
+    @GetMapping
     public String hello() {
         return "hello youtuber";
     }
